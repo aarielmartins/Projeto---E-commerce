c2068656a5c64a333eb1893a1856c94c01956bec
ariel <you@example.com>
2025-06-30 20:29:55 -0300

    criando a pagina de home

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,49 +1,29 @@
 import Product from '../Product'
 import { Container, List, Title } from './styles'
-import resident from '../../assets/images/resident.png'
+import Game from '../../models/games'
 
 export type Props = {
   title: string
   background: 'gray' | 'black'
+  games: Game[]
 }
 
-const ProductsList = ({ background, title }: Props) => (
+const ProductsList = ({ background, title, games }: Props) => (
   <Container background={background}>
     <div className="container">
       <Title>{title}</Title>
       <List>
-        <Product
-          category="Ação"
-          description="teste"
-          image={resident}
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Resident"
-        />
-        <Product
-          category="Ação"
-          description="teste"
-          image={resident}
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Resident"
-        />
-        <Product
-          category="Ação"
-          description="teste"
-          image={resident}
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Resident"
-        />
-        <Product
-          category="Ação"
-          description="teste"
-          image={resident}
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Resident"
-        />
+        {games.map((game) => (
+          <Product
+            key={game.id}
+            category={game.category}
+            description={game.description}
+            image={game.image}
+            infos={game.infos}
+            system={game.system}
+            title={game.title}
+          />
+        ))}
       </List>
     </div>
   </Container>
